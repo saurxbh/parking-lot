c12f8be7c7cac330155d1f1e7c817de4f5482628
saurxbh <36542272+saurxbh@users.noreply.github.com>
2025-07-08 13:33:29 -0400

    Add ParkingSpotService

--- src/main/java/dev/saurabh/parkinglot/service/ParkingSpotService.java
+++ src/main/java/dev/saurabh/parkinglot/service/ParkingSpotService.java
@@ -1,4 +1,10 @@
 package dev.saurabh.parkinglot.service;
 
+import dev.saurabh.parkinglot.entity.ParkingSpot;
+import dev.saurabh.parkinglot.enums.VehicleType;
+
+import java.util.Optional;
+
 public interface ParkingSpotService {
+    Optional<ParkingSpot> getAvailableSpot(VehicleType vehicleType);
 }
