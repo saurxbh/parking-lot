7e7c6f089319e32abe0c9f395a3c7455882fc433
saurxbh <36542272+saurxbh@users.noreply.github.com>
2025-07-08 14:02:28 -0400

    Add BillController

---
+++ src/main/java/dev/saurabh/parkinglot/controller/BillController.java
@@ -0,0 +1,21 @@
+package dev.saurabh.parkinglot.controller;
+
+import dev.saurabh.parkinglot.entity.Bill;
+import dev.saurabh.parkinglot.service.BillService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/bill")
+@RequiredArgsConstructor
+public class BillController {
+
+    private final BillService billService;
+
+    @PostMapping("/generate/{ticketId}")
+    public ResponseEntity<Bill> generateBill(@PathVariable Long ticketId) {
+        Bill bill = billService.generateBill(ticketId);
+        return ResponseEntity.ok(bill);
+    }
+}
