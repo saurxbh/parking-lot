1e108704868261d09988f7cc0c13cff8d3037a12
saurxbh <36542272+saurxbh@users.noreply.github.com>
2025-07-10 12:29:23 -0400

    Update methods in VehicleRepository

--- src/main/java/dev/saurabh/parkinglot/repository/VehicleRepository.java
+++ src/main/java/dev/saurabh/parkinglot/repository/VehicleRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
-    Optional<Vehicle> findByVehicleNo(String vehicleNo);
+    Optional<Vehicle> findByVehicleNumber(String vehicleNo);
 }
